d148c7334a9980ba2db8de8c84c0ef947b11cd7b
Nhan Nguyen <nhan2000ts@gmail.com>
2024-09-27 15:36:17 +0700

    add source

---
+++ ComplimentGeneratorAPIv2/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using ComplimentGeneratorAPIv2.Model;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace ComplimentGeneratorAPIv2.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+        private readonly IMongoCollection<User> _userCollection;
+       public UserController(ILogger<UserController> logger, IMongoDatabase database)
+        {
+            _logger = logger;
+            _userCollection = database.GetCollection<User>(Environment.GetEnvironmentVariable("Db_CollectionName"));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> Get()
+        {
+            var compliments = await _userCollection.Find(_ => true).ToListAsync();
+            return Ok(compliments);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> Post(User compliment)
+        {
+            await _userCollection.InsertOneAsync(compliment);
+            return CreatedAtAction("Get", new { id = compliment.Id }, compliment);
+        }
+    }
+}
---
+++ ComplimentGeneratorAPIv2/Model/User.cs
@@ -0,0 +1,10 @@
+﻿using MongoDB.Bson;
+
+namespace ComplimentGeneratorAPIv2.Model
+{
+    public class User
+    {
+        public ObjectId Id { get; set; }
+        public string? Content { get; set; }
+    }
+}
